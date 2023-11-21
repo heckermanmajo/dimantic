5cbbe6923463c988ae60cdc8b26d5e76517b9e56
majo <hackermanmajo@gmail.com>
2023-11-21 16:13:02 +0100

    fix my dumbness

--- dialogue/index.php
+++ dialogue/index.php
@@ -159,11 +159,62 @@ try {
     }; # end match
   }
   else {
-    (require $_SERVER["DOCUMENT_ROOT"] . "/pages/index/login_reg_page.php")(
-      login_error: $login_error ?? null,
-      register_error: $register_error ?? null,
-      app: $app
-    );
+    ?>
+    <div class="w3-row">
+      <div class="w3-col m9 s9 l9">
+        <div class="w3-card-4 w3-margin w3-padding">
+          <h3>Dialoge</h3>
+
+          <div class="w3-center">
+            <img src="/res/dasein.png" width="300px">
+          </div>
+        </div>
+      </div>
+      <div class="w3-rest">
+
+        <form
+          class="w3-margin w3-padding w3-card-4"
+          method="post">
+          <input type="hidden" name="action" value="login">
+          <h4>Login</h4>
+          <?= ($login_error ?? null)?->get_error_card() ?>
+          <label>
+            <span><small>Username/Email</small></span>
+            <br>
+            <input type="text" name="username_or_email" value="">
+          </label><br><br>
+          <label>
+            <span><small>Password</small></span>
+            <br>
+            <input type="password" name="password" value="">
+          </label><br><br>
+          <button class="button" type="submit">Login</button>
+        </form>
+
+        <form class="w3-margin w3-padding w3-card-4" method="post">
+          <h4>Register</h4>
+          <?= ($register_error ?? null)?->get_error_card() ?>
+          <input type="hidden" name="action" value="register">
+          <label>
+            <span><small>Username</small></span>
+            <br>
+            <input type="text" name="username" value="">
+          </label><br><br>
+          <label>
+            <span><small>Email</small></span>
+            <br>
+            <input type="text" name="email" value="">
+          </label><br><br>
+          <label>
+            <span><small>Password</small></span>
+            <br>
+            <input type="password" name="password" value="">
+          </label><br><br>
+          <button class="button" type="submit">Register</button>
+        </form>
+      </div>
+    </div>
+    <?php
   }
 
   HtmlUtils::footer($app);
